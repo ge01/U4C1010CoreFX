4b041ff5262c9da00abe72eea8b6770233de4734
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-24 16:59:27 -0500

    Working with CoreFX Classes

---
+++ CoreFX.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var result = rnd.Next(10);
+            Console.WriteLine(result);
+
+            // pause to see the random number
+            Console.ReadLine();
+        }
+    }
+}
